2e69f3c4e4e2bf8de1628c369012688d42573905
Naoki Narisawa <naoki.swbnml7070@gmail.com>
2019-07-01 19:18:28 +0900

    update

--- src/DataCenter.py
+++ src/DataCenter.py
@@ -1,8 +1,48 @@
+import os
 import pandas as pd
+import numpy as np
 
 
 def read(fname):
-    z = []
-    u = []
+    """
 
-    return z, u
+    :param fname: the name of file
+    :type fname: str
+    :return: z, c, u, nfeature
+    """
+    data = pd.read_table(fname, sep=" ", header=None)
+    # data = data[:500]
+    measure = data[data[0]=="EDGE_SE2_XY"]
+    control = data[data[0]=="EDGE_SE2"]
+
+    # create control array u
+    u = np.array(control.iloc[:,3:6]).T
+
+    # create measurement array z
+    time = 0
+    tmp = 0
+    nfeature = 0
+    fdict = dict()
+    c = [[] for _ in range(u.shape[1])]
+    z = [np.array([[], []]) for _ in range(u.shape[1])]
+    for _, row in measure.iterrows():
+        if tmp != row[1]:
+            tmp = row[1]
+            time = tmp - nfeature - 1
+        if tmp <= row[2]:
+            fdict[row[2]] = nfeature
+            nfeature += 1
+        #print(row[2])
+        c[time].append(fdict[row[2]])
+        z[time] = np.hstack((z[time], np.array([[row[3]],[row[4]]])))
+
+    return z, u, c, nfeature
+
+
+if __name__ == '__main__':
+    fname = "../data/sample0/input.txt"
+    z, c, u, n = read(fname)
+    print(n)
+    print(c[:10])
+    for i in range(10):
+        print(z[i])
--- src/GraphSLAM.py
+++ src/GraphSLAM.py
@@ -1,25 +1,28 @@
 import numpy as np
 from numpy.linalg import inv
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 INF = 1e9
 
 
 class GraphSLAM:
 
-    def __init__(self, z, u, f):
+    def __init__(self, z, u, c, nfeature):
         """
 
-        :param z: 3 dimentional array which represents measurement amount.
-        :param u: 2 dimentional array which represents control amount.
-        :param f: sampling rate
+        :param z: 3d list which represents measurement amount.
+        :param u: 2d array which represents control amount.
+        :param c: 2d list which represents correspondence.
+        :param nfeature: # of features
         """
 
         self.z = z
         self.u = u
-        self.T = len(u)
-        self.f = f
-        self.nfeature = 0
-        self.tau = dict()
+        self.c = c
+        self.T = u.shape[1]
+        self.nfeature = nfeature
+        self.tau = [[] for _ in range(nfeature)]
 
         self.Rsigma1 = 0.1
         self.Rsigma2 = 0.1
@@ -28,46 +31,37 @@ class GraphSLAM:
 
         self.Qsigma1 = 0.1
         self.Qsigma2 = 0.1
-        self.Qsigma3 = 0.1
-        self.Q = np.diag((self.Qsigma1, self.Qsigma2, self.Qsigma3))
+        self.Q = np.diag((self.Qsigma1, self.Qsigma2))
 
         self.max_iter = 3
 
-    def predict(self, u, z):
-        c = np.random.randint(3)
-        mu = self._initialize(u)
-        omega, xi = self._linearize(u, z, c, mu)
-        omega_til, xi_til = self._reduce(omega, xi)
-        mu, sigma = self._solve(omega_til, xi_til, omega, xi)
+    def predict(self, method=0):
+        n = 3 * (self.T + 1)
+        mu = self._initialize(self.u)
         for _ in range(self.max_iter):
-            n = 3 * (self.T + 1)
-            omega, xi = self._linearize(u, z, c, mu[:n])
-            omega_til, xi_til = self._reduce(omega, xi)
-            mu, sigma = self._solve(omega_til, xi_til, omega, xi)
+            omega, xi = self._linearize(self.u, self.z, self.c, mu[:n])
+            omega_til, xi_til = self._reduce(omega, xi, method)
+            mu, sigma = self._solve(omega_til, xi_til, omega, xi, method)
 
         return mu
 
     def _initialize(self, u):
         """
         initialize the average pose vector mu.
-        :param u: 2 dimentional array which represents control amount.
-        :return: 2 dimentional array which represents average pose vector.
+        :param u: 2d array which represents control amount.
+        :return: 2d array which represents average pose vector.
         """
         mu = np.zeros((3, self.T+1), dtype=np.float)
-        d_mu = np.zeros(3)
 
         for t in range(self.T):
-            d_mu[0] = (- np.sin(mu[2,t-1]) + np.sin(mu[2,t-1]+u[1]/self.f)) * (u[0]/u[1])
-            d_mu[1] = (np.cos(mu[2,t-1]) - np.cos(mu[2,t-1]+u[1]/self.f)) * (u[0]/u[1])
-            d_mu[2] = u[1] / self.f
-
-            mu[:,t+1] = mu[:,t] + d_mu
+            mu[:,t+1] = self._func_g(u[:,t], mu[:,t])
 
         return mu
 
     def _linearize(self, u, z, c, mu):
-        omega = np.zeros((3*(self.T+1),3*(self.T+1)),dtype=np.float)
-        xi = np.zeros((3*(self.T+1),3*(self.T+1)), dtype=np.float)
+        size = 3 * (self.T + 1) + 2 * self.nfeature
+        omega = np.zeros((size, size), dtype=np.float)
+        xi = np.zeros(size, dtype=np.float)
 
         omega[0:3,0:3] += np.array([[INF,0,0],
                                     [0,INF,0],
@@ -90,12 +84,10 @@ class GraphSLAM:
         update info matrix and vector at x_t and m_j
         """
         for t in range(self.T):
-            for i in range(len(z[:,t])):
-                j = c[i,t]
-                if j in self.tau.keys():
-                    self.tau[j].append(t)
-                else:
-                    self.tau[j] = [t]
+            for i in range(z[t].shape[1]):
+                j = c[t][i]
+                self.tau[j].append(t)
+
                 z_hat = self._func_h(mu[:,t+1], mu[:,j])
                 H = self._jacobi_h(mu[:,t+1], mu[:,j])
 
@@ -103,7 +95,8 @@ class GraphSLAM:
                 pos_m = 3*(self.T+1) + 2*j
 
                 C = H.T @ inv(self.Q) @ H
-                D = H.T @ inv(self.Q) @ (z[:,t]-z_hat+H@np.hstack((mu[:,t+1], mu[:,j])))
+                D = H.T @ inv(self.Q) @ (z[t][:,i]-z_hat+H@np.hstack((mu[:,t+1], mu[:2,j])))
+                """
                 if not j < self.nfeature:
                     size = 3*(self.T+1) + 2*(j+1)
                     tmp_mat = np.zeros((size,size))
@@ -113,6 +106,7 @@ class GraphSLAM:
                     tmp_vec[:xi.shape[0]] = xi
                     xi = tmp_vec
                     self.nfeature = j+1
+                """
                 omega[pos_x:pos_x+3,pos_x:pos_x+3] += C[:3,:3]
                 omega[pos_x:pos_x+3,pos_m:pos_m+2] += C[:3,3:5]
                 omega[pos_m:pos_m+2,pos_x:pos_x+3] += C[3:5,:3]
@@ -122,78 +116,93 @@ class GraphSLAM:
 
         return omega, xi
 
-    def _reduce(self, omega, xi):
+    def _reduce(self, omega, xi, method=0):
         n = 3*(self.T + 1)
         omega_til = omega[:n, :n]
         xi_til = xi[:n]
 
-        for j in range(self.nfeature):
-            omega_til -= omega[:n, n+2*j:n+2*j+2] @ omega[n+2*j:n+2*j+2, n+2*j:n+2*j+2] @ omega[n+2*j:n+2*j+2, :n]
-            xi_til -= omega[:n, n+2*j:n+2*j+2] @ omega[n+2*j:n+2*j+2, n+2*j:n+2*j+2] @ xi[:n]
+        if method == 0:
+            omega_til -= omega[:n, n:] @ inv(omega[n:, n:]) @ omega[n:, :n]
+            xi_til -= omega[:n, n:] @ inv(omega[n:, n:]) @ xi[n:]
+        elif method == 1:
+            for j in tqdm(range(self.nfeature)):
+                jrange = [n+2*j, n+2*j+1]
+                beta = omega[:n, :][:, jrange]
+                alpha = beta @ inv(omega[jrange, :][:, jrange])
+                omega_til -= np.dot(alpha, beta.T)
+                xi_til -= alpha @ xi[jrange]
         return omega_til, xi_til
 
-    def _solve(self, omega_til, xi_til, omega, xi):
-        n = 3 *(self.T + 1)
+    def _solve(self, omega_til, xi_til, omega, xi, method=0):
+        n = 3 * (self.T + 1)
 
         mu = np.zeros_like(xi)
         sigma = inv(omega_til)
         mu[:n] = sigma @ xi_til
 
-        for j in range(self.nfeature):
-            sigma_j = inv(omega[n+2*j:n+2*j+2, n+2*j:n+2*j+2])
-            rang = np.hstack((self.tau[j], self.tau[j]+1, self.tau[j]+2))
-            mu[2*j:2*j+2] = sigma_j @ (xi[2*j:2*j+2] + sigma[2*j:2*j+2,:][:,rang] @ mu[rang])
+        if method == 0:
+            sigma_m = inv(omega[n:, n:])
+            mu[n:] = sigma_m @ (xi[n:] + omega[n:, :n] @ xi_til)
+        elif method == 1:
+            for j in range(self.nfeature):
+                self.tau[j] = np.array(self.tau[j])
+                jrange = [n+2*j, n+2*j+1]
+                sigma_j = inv(omega[jrange, :][:, jrange])
+                tau = np.hstack((3*self.tau[j], 3*self.tau[j]+1, 3*self.tau[j]+2))
+                mu[n+2*j:n+2*j+2] = sigma_j @ (xi[jrange] + omega[jrange, :][:, tau] @ mu[tau])
         return mu, sigma
 
-    def _correspondence_test(self, omega, mu, sigma, j, k):
-        tau_jk = np.hstack((self.tau[j], self.tau[j]+1, self.tau[j]+2, self.tau[k], self.tau[k]+1, self.tau[k]+2))
-        jk = np.array([j, j+1, k, k+1]) + 3*(self.T + 1)
-
-        omega_jk = omega[jk,:][:,jk] - omega[jk,:][:,tau_jk] @ sigma[tau_jk,:][:,tau_jk] @ omega[tau_jk,:][:,jk]
-        xi_jk = omega_jk @ mu[jk]
-
+    def _visualize(self):
         return 0
 
     # 運動学動作モデル g
     def _func_g(self, u, x_prev):
+        x = np.zeros(3, dtype=np.float)
+        """
         d_x = np.zeros(3)
         d_x[0] = (- np.sin(x_prev[2]) + np.sin(x_prev[2] + u[1] / self.f)) * (u[0] / u[1])
         d_x[1] = (np.cos(x_prev[2]) - np.cos(x_prev[2] + u[1] / self.f)) * (u[0] / u[1])
         d_x[2] = u[1] / self.f
-
-        x = x_prev + d_x
+        """
+        x[:2] = x_prev[:2] + self._rotation_matrix(x_prev[2]) @ u[:2]
+        x[2] = x_prev[2] + u[2]
 
         return x
 
     # 関数gのxに対するヤコビ行列
     def _jacobi_g(self, u, x):
+        """
         G = np.eye(3)
 
         G[0,2] = (- np.cos(x[2]) + np.cos(x[2] + u[1] / self.f)) * (u[0] / u[1])
         G[1,2] = (- np.sin(x[2]) + np.sin(x[2] + u[1] / self.f)) * (u[0] / u[1])
+        """
+
+        G = np.eye(3, dtype=np.float)
+        P = np.array([[-np.sin(x[2]), -np.cos(x[2])],
+                      [np.cos(x[2]), -np.sin(x[2])]])
+        G[:2,2] = P @ u[:2]
 
         return G
 
     # 計測関数 h
-    def _func_h(self, x, m, predict=False):
-        z = np.zeros(3, dtype=np.float)
+    def _func_h(self, x, m):
+        z = np.zeros(2, dtype=np.float)
         Rot = self._rotation_matrix(-x[2])
 
         z[:2] = Rot @ (m[:2] - x[:2])
-        z[2] = 0 if predict else m[2]
 
         return z
 
     # 関数hの[x_t, m_j]におけるヤコビ行列
     def _jacobi_h(self, x, m):
-        h = np.zeros((3,6))
+        h = np.zeros((2,5))
         Rot = self._rotation_matrix(x[2])
         P = np.array([[-np.sin(x[2]), -np.cos(x[2])],
                       [np.cos(x[2]), -np.sin(x[2])]])
         h[:2,:2] = -1 * Rot
         h[:2,3:5] = Rot
         h[:2,2] = P @ (m[:2] - x[:2])
-        h[2,5] = 1
 
         return h
 
@@ -203,3 +212,20 @@ class GraphSLAM:
 
         return Rot
 
+    def test(self):
+        n = 3 * (self.T + 1)
+        mu = self._initialize(self.u)
+        plt.scatter(mu[0,:], mu[1,:])
+        plt.show()
+        # print(mu)
+        # omega, xi = self._linearize(self.u, self.z, self.c, mu[:n])
+        # print(omega)
+        # print(xi)
+        # omega_til, xi_til = self._reduce(omega, xi, method=0)
+        # print(omega_til)
+        # print(xi_til)
+        # mu, sigma = self._solve(omega_til, xi_til, omega, xi, method=0)
+        # print(mu)
+        # print(sigma)
+        # plt.scatter(mu[[3*i for i in range(self.T+1)]], mu[[3*i+1 for i in range(self.T+1)]])
+        # plt.show()
